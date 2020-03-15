68f996ef629f5e107c95047dfcfca053605ccd7b
aaron_beach <aaron.r.beach@gmail.com>
2020-03-14 23:27:26 -0400

    add returns zero for empty str

--- sample_project/code.py
+++ sample_project/code.py
@@ -1,2 +1,25 @@
 def greeting():
-    return 'Hello, World!'
+    return "Hello, World!"
+
+
+class StringCalculator:
+    def add(number: str) -> int:
+        return 0
+
+
+"""
+   1. The method can take 0, 1 or 2 numbers, and will
+   return their sum (for an empty string it will return 0)
+   for example
+   `“” == 0 , “1” == 1 , “1,2” == 3`
+
+
+   2. Start with the **simplest** test case of an **empty string** and move to **one & two numbers**
+
+
+   3. Remember to solve things as **simply as possible** so that you force yourself to
+   write tests you did not think about
+
+
+   4. Remember to **refactor** after each passing test
+"""
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,3 +4,7 @@ from sample_project import code
 def test_hello():
     assert code.greeting() != ""
     assert code.greeting() == "Hello, World!"
+
+
+def test_string_calculator_return_zero_empty_string():
+    assert code.StringCalculator.add("") == 0
