ab6529f11f6df7a7d6833c5c7b2d1bba08a96fa4
aaron_beach <aaron.r.beach@gmail.com>
2020-03-15 02:35:19 -0400

    passing \n

--- sample_project/code.py
+++ sample_project/code.py
@@ -1,35 +1,38 @@
-def greeting():
-    return "Hello, World!"
+import re
+
+"""
+1.
+    create a simple class `class StringCalculator`
+    with a method `public int Add(string numbers)`
+
+   1. The method can take 0, 1 or 2 numbers, and will
+   return their sum (for an empty string it will return 0)
+   for example
+   `“” == 0 , “1” == 1 , “1,2” == 3`
+"""
 
 
 class StringCalculator:
     def add(number: str) -> int:
+
+        """
+        2. Start with the **simplest** test case of an
+        **empty string** and move to **one & two numbers**
+        """
         if len(number) < 1:
             if number == "":
                 return 0
             else:
                 return int(number)
         else:
-            numbers = number.split(",")
+            """
+            allow \n to replace ','
+            use regex with split to give list
+            """
+
+            pattern = r"[,:\n]"
+            numbers = re.split(pattern, number)
             result = 0
             for i in numbers:
                 result += int(i)
             return result
-
-
-"""
-   1. The method can take 0, 1 or 2 numbers, and will
-   return their sum (for an empty string it will return 0)
-   for example
-   `“” == 0 , “1” == 1 , “1,2” == 3`
-
-
-   2. Start with the **simplest** test case of an **empty string** and move to **one & two numbers**
-
-
-   3. Remember to solve things as **simply as possible** so that you force yourself to
-   write tests you did not think about
-
-
-   4. Remember to **refactor** after each passing test
-"""
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,9 @@
 from sample_project import code
 
 
-def test_hello():
-    assert code.greeting() != ""
-    assert code.greeting() == "Hello, World!"
+"""
+   2. Start with the **simplest** test case of an **empty string** and move to **one & two numbers**
+"""
 
 
 def test_string_calculator_return_zero_empty_string():
@@ -21,3 +21,21 @@ def test_string_calc_return_sum_of_two_numbers():
     assert code.StringCalculator.add("1,4") == 5
     assert code.StringCalculator.add("8,9") == 17
     assert code.StringCalculator.add("3,1") == 4
+
+
+"""
+2. Allow the **Add** method to handle an unknown amount of numbers
+"""
+
+
+def test_str_calc_return_sum_of_multiple_numbers():
+    assert code.StringCalculator.add("8,0,0") == 8
+    assert code.StringCalculator.add("8,10,2") == 20
+    assert code.StringCalculator.add("1,1,2") == 4
+    assert code.StringCalculator.add("9,1,4") == 14
+    assert code.StringCalculator.add("3,10,9") == 22
+    assert code.StringCalculator.add("10,100,1") == 111
+
+
+def test_str_calc_new_line_add():
+    assert code.StringCalculator.add("1:2\n1") == 4
