b08c1c2eafcd15034ef7b86ee42e16118a3e571f
aaron_beach <aaron.r.beach@gmail.com>
2020-03-16 00:11:39 -0400

    add commenting to code page

--- sample_project/code.py
+++ sample_project/code.py
@@ -9,14 +9,20 @@ import re
     Add(numbers)
         receives numbers as string
         returns int
+    GetCalledCount()
+        increases each add() call
+        returns int
 
-        Helper methods
-        -   find_numbers(num)
-                receives numbers as string
-                returns list of strings
-        -   sum_numbers(list_str_nums)
-                receives list as strings
-                returns int sum
+    static methods
+    -   find_numbers(num)
+            receives numbers as string
+            returns list of strings
+    -   sum_numbers(list_str_nums)
+            receives list as strings
+            returns int sum
+    -   is_positive(num)
+            receives number
+            raise error if negative
 """
 
 
@@ -24,9 +30,17 @@ class StringCalculator:
     def __init__(self, count):
         self.count = count
 
+    """
+    count add() calls
+    """
+
     def GetCalledCount(self):
         self.count += 1
         return self.count
+
+    """
+    parse string into List of ints
+    """
 
     @staticmethod
     def find_numbers(str_nums: str):
@@ -36,11 +50,19 @@ class StringCalculator:
         else:
             return re.findall(r"-?[0-9]+", str_nums)
 
+    """
+    error validation if negative raise error
+    """
+
     @staticmethod
     def is_positive(num):
         if int(num) < 0:
-
             raise ValueError("negatives not allowed")
+
+    """
+    calculate the sum of the values
+    includes error checking
+    """
 
     @staticmethod
     def sum_numbers(list_str_nums):
@@ -51,10 +73,14 @@ class StringCalculator:
                 if num.isdigit():
                     if int(num) <= 1000:
                         sum_of_numbers += int(num)
-
             except ValueError:
                 print("negatives not allowed: " + num)
         return sum_of_numbers
+
+    """
+    add method. Take a string of numbers add them.
+        add calls static methods
+    """
 
     def add(self, numbers: str) -> int:
         StringCalculator.GetCalledCount(self)
@@ -63,6 +89,11 @@ class StringCalculator:
             return StringCalculator.sum_numbers(list_strings)
         else:
             return 0 if numbers == "" else int(numbers[0])
+
+
+"""
+main test
+"""
 
 
 def main():
